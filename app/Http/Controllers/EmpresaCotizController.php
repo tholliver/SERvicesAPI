3301db7f1ff7890c4fb3d69db8e2b6b74bfc749f
tholliver <michaelmmv@outlook.com>
2021-06-08 11:35:41 -0400

    + mini cambiios

--- app/Http/Controllers/EmpresaCotizController.php
+++ app/Http/Controllers/EmpresaCotizController.php
@@ -70,7 +70,7 @@ class EmpresaCotizController extends Controller
          $nombre = "pdf_".time().".".$file->getClientOriginalExtension();
          $rute1 = $request->file("cotizacion_pdf")->move(public_path()."/pdf", $nombre); //Moving the file to public route
         
-         $ruta = 'public/pdf/'.$nombre;
+         $ruta = public_path()."/".'pdf/'.$nombre;
          //$request->file("cotizacion_pdf")->move(public_path("pdf/".$nombre));
    
         $empresaCot = EmpresaCotizacion::Find($id);
